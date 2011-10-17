08c49e6c41054f8771734197bf3f9d2f346f160b
Felix Rabe <public@felixrabe.net>
2011-10-17 17:24:27 +0200

    My style...

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = module.exports = express.createServer();
 
 // Configuration
 
-app.configure(function(){
+app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
@@ -21,17 +21,17 @@ app.configure(function(){
   app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
+app.configure('development', function() {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
-app.configure('production', function(){
+app.configure('production', function() {
   app.use(express.errorHandler()); 
 });
 
 // Routes
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.render('index', {
     title: 'My New Application'
   });
