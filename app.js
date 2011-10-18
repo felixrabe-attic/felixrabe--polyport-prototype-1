ea6bc974c77da45808f21305985cb4dfcb44af19
Felix Rabe <public@felixrabe.net>
2011-10-18 12:04:31 +0200

    My style #2

--- app.js
+++ app.js
@@ -10,36 +10,36 @@ var app = module.exports = express.createServer();
 // Configuration
 
 app.configure(function() {
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' }));
-  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: 'your secret here' }));
+    app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function() {
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function() {
-  app.use(express.errorHandler()); 
+    app.use(express.errorHandler()); 
 });
 
 // Routes
 
 app.get('/', function(req, res) {
-  res.render('index', {
-    title: 'My New Application'
-  });
+    res.render('index', {
+        title: 'My New Application'
+    });
 });
 
 app.post('/login', function(req, res) {
-  res.header('Content-Type', 'text/plain');
-  res.send(req.body);
+    res.header('Content-Type', 'text/plain');
+    res.send(req.body);
 });
 
 var port = process.env.PORT || 3000;
