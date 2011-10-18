5332b10617bdc88170dd0c31e49b359b374de39b
Felix Rabe <public@felixrabe.net>
2011-10-18 11:37:45 +0200

    Change cookie session secret

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.configure(function() {
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(express.cookieParser());
-    app.use(express.session({ secret: 'your secret here' }));
+    app.use(express.session({ secret: 'If only you knew' }));
     app.use(require('stylus').middleware({ src: __dirname + '/public' }));
     app.use(app.router);
     app.use(express.static(__dirname + '/public'));
